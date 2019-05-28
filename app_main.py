bc67c01bc2bd10d330a2550cd77e869af3a98610
William Barrilli <felip.ew@hotmail.com>
2019-05-28 15:21:34 -0300

    exemplo de func com mongo(alunos)

--- app_main.py
+++ app_main.py
@@ -2,6 +2,7 @@ import pymongo
 from flask import Flask
 from flask import jsonify
 from flask import request
+
 from models.aluno import Aluno
 from models.aluno_schema import AlunoSchema
 from models.curso import Curso
@@ -14,16 +15,13 @@ from dao.db import db
 app = Flask(__name__)
 
 
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+mongo = PyMongo(app)
 # BASE_URL = "/api/components/schemas"
-BASE_URL = "/api"
+# BASE_URL = "/api"
 
-aluno_1 = Aluno('1', 'teste', 'email', 'teste1@teste.com', 'Email teste1')
-aluno_2 = Aluno('2', 'teste2', 'email', 'teste2@teste.com', 'Email teste2')
-aluno_3 = Aluno('3', 'teste3', 'email', 'teste3@teste.com', 'Email teste3')
-aluno_4 = Aluno('4', 'teste4', 'email', 'teste4@teste.com', 'Email teste4')
-
-
-alunos = [aluno_1, aluno_2, aluno_3, aluno_4]
 messages = {
     "empty": {"message": "Dados vazios"},
     "none": {"message": "aluno nao encontrado!"},
@@ -38,56 +36,84 @@ def index():
     return "API Dibra!"
 
 
-@app.route(BASE_URL + "/aluno", methods=['GET'])
-def get_alunos():
-    print(len(alunos))
-    if len(alunos) > 0:
-        return jsonify(AlunoSchema(many=True).dump(alunos))
-    return jsonify(messages["empty"])
+@app.route("/aluno", methods=['GET'])
+def get_all_alunos():
+    alunos = mongo.db.alunos
+    retorno = []
+    for aluno_obj in alunos.find():
+        retorno.append(
+            {'id': aluno_obj['id'], 'name': aluno_obj['name'], 'canal': aluno_obj['canal'], 'valor': aluno_obj['valor'], 'obs': aluno_obj['obs']})
+        if len(retorno) == 0:
+            return jsonify({"message": "Não há alunos cadastrados!"})
+
+    return jsonify({'alunos': retorno})
 
 
-@app.route(BASE_URL + "/aluno/<aluno_id>", methods=['GET'])
+
+@app.route("/aluno/<aluno_id>", methods=['GET'])
 def get_aluno(aluno_id=None):
-    print(aluno_id)
-    for aluno in alunos:
-        if aluno.id == aluno_id:
-            return jsonify(AlunoSchema().dump(aluno))
+    alunos = mongo.db.alunos
+    aluno_obj = alunos.find_one({'name': name})
+    if not aluno_obj:
+        retorno = "aluno não encontrado"
+        return jsonify(retorno)
+    aluno_obj.pop('_id')
+    return jsonify(aluno_obj)
+    
 
-    return jsonify(messages["empty"])
-
-
-@app.route(BASE_URL + "/aluno", methods=['POST'])
+@app.route("/aluno", methods=['POST'])
 def create_aluno():
-    request_data = request.get_json()
-    new_aluno = Aluno(str(len(alunos) + 1), request_data['nome'],
-                      request_data['sobrenome'], request_data['cpf'],
-                      request_data['data_nascimento'])
-    alunos.append(new_aluno)
-    return jsonify(messages["created"])
+    alunos = mongo.db.alunos
+    name = request.json['name']
+    canal = request.json['canal']
+    valor = request.json['valor']
+    obs = request.json['obs']
+    id = gera_id()
+
+    alunos.insert({
+        'id': id,
+        'name': name,
+        'canal': canal,
+        'valor': valor,
+        'obs': obs})
+
+    return jsonify({'result': 'ok'})
 
 
-@app.route(BASE_URL + "/aluno/<aluno_id>", methods=['PUT'])
-def update_aluno(aluno_id=None):
-    request_data = request.get_json()
-    for aluno in alunos:
-        if aluno.id == aluno_id:
-            aluno.nome = request_data['nome']
-            aluno.sobrenome = request_data['sobrenome']
-            aluno.cpf = request_data['cpf']
-            aluno.data_nascimento = request_data['data_nascimento']
-            return jsonify(messages["updated"])
-    return jsonify(messages["empty"])
+@app.route("/aluno/<aluno_id>", methods=['PUT'])
+def set_aluno_name(aluno_id):
+    request_data = request.get_json()   
+    new_name = request_data['name']
+    canal = request_data['canal']
+    valor = request_data['valor']
+    obs = request_data['obs']
+    mongo.db.alunos.update_one(
+        {"name": name},
+        {
+            "$set": {
+                "name": new_name,
+                "canal": canal,
+                "valor": valor,
+                "obs": obs
+            }
+        }
+    )
+    
+    return jsonify({"message": "Só por Deus"})
+
+@app.route("/aluno/<aluno_id>", methods=['DELETE'])
+def delete_aluno(aluno_id):
+    mongo.db.alunos.remove({'name': name})
+
+    alunos = mongo.db.alunos
+    aluno_obj = alunos.find_one({'name': name})
+    if aluno_obj != None:
+        retorno = "Error"
+    else:
+        retorno = "aluno excluido"
+    return jsonify({'mensagem': retorno})
 
 
-@app.route(BASE_URL + "/aluno/<aluno_id>", methods=['DELETE'])
-def delete_aluno(aluno_id=None):
-    for aluno in alunos:
-        if aluno.id == aluno_id:
-            print(alunos.index(aluno))
-            alunos.pop()
-            return jsonify(messages["deleted"])
-        else:
-            return jsonify(messages["none"])
 
 
 # --------------CURSO
